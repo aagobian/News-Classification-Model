54567a25f411ec9a0e7b2d41964f7487ff631c98
Arees Agobian <areesagob@gmail.com>
2024-07-18 23:02:26 -0400

    Simplify Naive Bayes model into pipeline

--- main.py
+++
@@ -1,3 +0,0 @@
-from src.preprocessing import preprocessing
-
-df = preprocessing()
--- src/Naive_Bayes.py
+++ src/Naive_Bayes.py
@@ -2,6 +2,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.pipeline import Pipeline
 import pandas as pd
 
 # read dataset
@@ -14,21 +15,17 @@ Y = df["label"]
 # split data into training and testing data
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.25, random_state=15)
 
-# initialize TF-IDF vectorizer
-tfidf = TfidfVectorizer(stop_words="english", max_df=0.7)
+# create pipeline
+pipeline = Pipeline([
+    ('tfidf', TfidfVectorizer(stop_words="english", max_df=0.7)), # initialize TF-IDF vectorizer
+    ('nb', MultinomialNB()) # create and fit Naive Bayes model
+])
 
-# fit TF-IDF on training data
-train_tfidf = tfidf.fit_transform(X_train)
+# fit pipeline on training data
+pipeline.fit(X_train, Y_train)
 
-# transform test data using the fitted TF-IDF vectorizer
-test_tfidf = tfidf.transform(X_test)
-
-# create and fit Naive Bayes model
-nb = MultinomialNB()
-nb.fit(train_tfidf, Y_train)
-
-# predict on test data
-nb_pred = nb.predict(test_tfidf)
+# predict on testing data
+nb_pred = pipeline.predict(X_test)
 
 # print classification report and confusion matrix
 print("Naive Bayes Classification Report:")
