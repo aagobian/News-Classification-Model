f64317d166b5d1edad506ced33855c084a59191d
Arees Agobian <areesagob@gmail.com>
2024-07-18 20:49:20 -0400

    Completely rewrite preprocessing code to simplify

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,62 +1,15 @@
-from nltk.tokenize import sent_tokenize, word_tokenize
-from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer
 import pandas as pd
-import re
-from tqdm import tqdm
 
-def preprocessing():
-    # Read data
-    df = pd.read_csv("data/raw/WELFake.csv")
+# Read dataset, drop NA values, and concatenate title and text columns
+df = pd.read_csv("data/raw/WELFake.csv").dropna()
+df["text"] = df["title"] + ' ' + df["text"]
 
-    # Drop NA values
-    df = df.dropna()
+# remove title and unnamed column
+df.drop(columns=["title"], inplace=True)
+df.drop(columns=["Unnamed: 0"], inplace=True)
 
-    # Initialize tqdm for monitoring progress
-    tqdm.pandas()
+# lowercase text for uniformity
+df["text"] = df["text"].apply(lambda x: x.lower())
 
-    # Process title and text columns
-    for col in ["title", "text"]:
-        # Apply tqdm on pandas series
-        tqdm.pandas(desc=f"Processing {col} column...")
-        
-        print(df.head())
-        print("Tokenizing sentences...")
-
-        # Perform segmentation
-        df[col] = df[col].progress_apply(sent_tokenize)
-
-        print(df.head())
-        print("Removing punctuation...")
-
-        # Remove punctuation
-        df[col] = df[col].progress_apply(lambda x: [re.sub(r"[^a-zA-Z0-9]", " ", sentence) for sentence in x])
-        
-        print(df.head())
-        print("Tokenizing words...")
-
-        # Tokenize words
-        df[col] = df[col].progress_apply(lambda x: [word_tokenize(sentence) for sentence in x])
-        
-        print(df.head())
-        print("Removing stop words...")
-        
-        # Remove stop words
-        stop_words = set(stopwords.words("english"))
-        df[col] = df[col].progress_apply(lambda x: [[word for word in sentence if word.lower() not in stop_words] for sentence in x])
-        
-        print(df.head())
-        print("Lemmatizing words...")
-
-        # Lemmatize words
-        lemmatizer = WordNetLemmatizer()
-        df[col] = df[col].progress_apply(lambda x: [[lemmatizer.lemmatize(word) for word in sentence] for sentence in x])
-        
-        print(df.head())
-        
-    # Save preprocessed data
-    print("Saving processed data...")
-    df.to_csv("data/processed/WELFakeProcessed.csv", mode="w", index=False)
-
-if __name__ == "__main__":
-    preprocessing()
+# write to new file
+df.to_csv("data/processed/WELFakeProcessed.csv", mode= "w", index=False)
